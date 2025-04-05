87f680a39c59b994b4438c55347e2d5eba660e2a
Dee-24-tu <159555226+Dee-24-tu@users.noreply.github.com>
2025-04-05 23:31:38 +0000

    correct typo errors

--- bin/scripts/backup-intranet.sh
+++ bin/scripts/backup-intranet.sh
@@ -2,8 +2,10 @@
 
 # Create a variable to be the signiture stamp for filename purposes
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
+
 BACKUP_SOURCE_PATH="/var/www/html/Intranet/"
-sudo mkdir /var/bin/Backups &>/dev/null # Create a backup directory if it does not exist
+
+sudo mkdir /var/www/html/ackups &>/dev/null # Create a backup directory if it does not exist
 BACKUP_DESTINATION="/var/bin/Backups/Intranet-backup-$timestamp.zip"
 
 # Change permissions so no one can edit the file
