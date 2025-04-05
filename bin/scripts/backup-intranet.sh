da1aa74190ee85baa7224c76f20dea3755702ee4
Dee-24-tu <159555226+Dee-24-tu@users.noreply.github.com>
2025-04-05 23:38:09 +0000

    zip the backed up folder and move to back up file

--- bin/scripts/backup-intranet.sh
+++ bin/scripts/backup-intranet.sh
@@ -5,14 +5,16 @@ timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
 BACKUP_SOURCE_PATH="/var/www/html/Intranet/"
 
-sudo mkdir /var/www/html/ackups &>/dev/null # Create a backup directory if it does not exist
-BACKUP_DESTINATION="/var/bin/Backups/Intranet-backup-$timestamp.zip"
+sudo mkdir /var/www/html/intranet_backup/backup_2025 &>/dev/null # Create a backup directory if it does not exist
+BACKUP_DESTINATION="/var/www/html/intranet_backup/backup_2025/Intranet-backup-$timestamp.zip"
 
 # Change permissions so no one can edit the file
 sudo chmod -R 000 $BACKUP_SOURCE_PATH
 
 # Save backup into a compressed file with time stamp on it and move to another folder
-echo "Backing up $BACKUP_DESTINATION $BACKUP_SOURCE_PATH
+echo "Backing up  $BACKUP_SOURCE_PATH into $BACKUP_DESTINATION 
+
+sudo zip -r -q $BACKUP_DESTINATION $BACKUP_SOURCE_PATH
 
 # Change permission back to normal
 sudo chmod -R 744 $BACKUP_SOURCE_PATH
